52f8a970c78367e09ff4e6b3d4fde82b694c1a58
sgshs <hsson@mind-one.co.kr>
2023-10-17 21:40:39 +0900

    경로 오류 수정

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -14,15 +14,15 @@ const App = () => {
 
   return (
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profiles/:username" element={<Profile />} />
-          <Route path="/articles" element={<Articles />}>
+        <Route path="./" element={<Layout />}>
+          <Route path="./" element={<Home />} />
+          <Route path="./about" element={<About />} />
+          <Route path="./profiles/:username" element={<Profile />} />
+          <Route path="./articles" element={<Articles />}>
             <Route path=":id" element={<Article />} />
           </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/mypage" element={<MyPage />} />
+          <Route path="./login" element={<Login />} />
+          <Route path="./mypage" element={<MyPage />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
