ff32742b7eabd7652dc0e83cb8e46987cfc26a45
sgshs <hsson@mind-one.co.kr>
2023-10-17 15:13:25 +0900

    next / react init

---
+++ testreact/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="./reacttest2023101701"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          2023-10-17 part1. 리액트 시작하기
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
