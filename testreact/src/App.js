971e503ac5c9145c52131234a4ddd2e4b64a3d74
sgshs <hsson@mind-one.co.kr>
2023-10-17 21:48:27 +0900

    .

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -8,21 +8,20 @@ import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
 import MyPage from './pages/MyPage';
-import { Navigate } from 'react-router-dom';
 
 const App = () => {
 
   return (
       <Routes>
-        <Route path="./" element={<Layout />}>
-          <Route path="./" element={<Home />} />
-          <Route path="./about" element={<About />} />
-          <Route path="./profiles/:username" element={<Profile />} />
-          <Route path="./articles" element={<Articles />}>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles/:username" element={<Profile />} />
+          <Route path="/articles" element={<Articles />}>
             <Route path=":id" element={<Article />} />
           </Route>
-          <Route path="./login" element={<Login />} />
-          <Route path="./mypage" element={<MyPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<MyPage />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
